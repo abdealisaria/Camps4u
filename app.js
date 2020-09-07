d8cb0f90ef71a0c72f4e4d76af41e6a1635e3b5f
Abdeali Saria <abdealisaria@gmail.com>
2020-09-07 20:53:50 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 // require("dotenv").config();
 // require packages
 var express = require("express"),
-    app = express(),
-    bodyparser = require("body-parser"),
-    mongoose = require("mongoose"),
-    flash = require("connect-flash"),
-    methodoverride = require("method-override"),
-    camps4umodel = require("./models/campsite"),
-    commentmodel = require("./models/comment"),
-    passport = require("passport"),
-    localstrategy = require("passport-local"),
-    usermodel = require("./models/user"),
-    seedDB = require("./seeds");
+  app = express(),
+  bodyparser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  methodoverride = require("method-override"),
+  camps4umodel = require("./models/campsite"),
+  commentmodel = require("./models/comment"),
+  passport = require("passport"),
+  localstrategy = require("passport-local"),
+  usermodel = require("./models/user"),
+  seedDB = require("./seeds"),
+  port = process.env.PORT;
 // require routes
 var commentroutes = require("./routes/comments"),
-    campsiteroutes = require("./routes/campsites"),
-    indexroutes = require("./routes/index");
+  campsiteroutes = require("./routes/campsites"),
+  indexroutes = require("./routes/index");
 // app setup
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
-mongoose.connect("mongodb+srv://abd:abd@cluster0-zvc42.mongodb.net/camps4u?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://abd:abd@cluster0-zvc42.mongodb.net/camps4u?retryWrites=true&w=majority"
+);
 
 // localdb
 // mongoose.connect("mongodb://localhost/camps4udb");
@@ -33,11 +36,13 @@ app.locals.moment = require("moment");
 // seedDB(); seed the database
 
 // passport setup
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "abdealisaria",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localstrategy(usermodel.authenticate()));
@@ -45,10 +50,10 @@ passport.serializeUser(usermodel.serializeUser());
 passport.deserializeUser(usermodel.deserializeUser());
 
 app.use(function (req, res, next) {
-    res.locals.currentuser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+  res.locals.currentuser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 // use routes
 app.use(indexroutes);
@@ -56,6 +61,6 @@ app.use(commentroutes);
 app.use(campsiteroutes);
 
 // express listen for requests
-app.listen(3000, function () {
-    console.log("The Camps4U server has started....");
+app.listen(port, function () {
+  console.log("The Camps4U server has started at " + port);
 });
